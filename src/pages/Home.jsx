e6195b52a97c7f75c27de3c96684e1f7db7fcb33
Rajan Vaishnav <1574@silvertouch.local>
2025-02-26 16:51:48 +0530

    main banner ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
 
+
+
 const Home = () => {
   return (
   <main className="hero-section main">
     <div className="container grid grid-two-cols">
-            <div className="hero-content">
+            <div className="hero-content align-items-center d-flex flex-wrap">
+              <div>
                     <h1 className="heading-xl">
                     Explore the world, one country at a time.  
                     </h1>  
@@ -14,9 +17,12 @@ const Home = () => {
                     <button className="btn btn-darken btn-inline bg-white-box">
                       Start Exploring <FaLongArrowAltRight /> 
                     </button>
+                    </div>
             </div>
-            <div className="hero-image">
-
+            <div className="hero-image align-items-center d-flex">
+                  <img src="/images/earth.png" alt="world beauty"
+                  className="bannerImage"
+                   />
             </div>
     </div>
   </main>
