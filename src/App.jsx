24a1333c4392d51b02a8db091f6d5c8239c6135a
Rajan Vaishnav <1574@silvertouch.local>
2025-02-26 10:30:50 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./components/Layout/AppLayout";
+
 import "./App.css";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Country from "./pages/Country";
-import Contact from "./pages/Contact";
+import Contact from "./pages/Contact";  
 
 
 const router = createBrowserRouter([
+
+
   {
     path:"/",
-    element:<Home />,
-
+    element:<AppLayout  />,
+    children: [
+      { path:"/", element:<Home /> },   
+      { path:"/about", element:<About /> },
+      { path:"/country", element:<Country /> },
+      { path:"/contact", element:<Contact /> },
+    ],
   },
-  {
-    path:"/about",
-    element:<About />,  
-  },
-  {
-    path:"/country",
-    element:<Country  />,  
-  },
-  {
-    path:"/contact",
-    element:<Contact />,  
-  },    
+   
 ]);
 
 
---
+++ src/components/Layout/AppLayout.jsx
@@ -0,0 +1,12 @@
+import { Headers } from '../UI/Headers';
+import { Footers } from '../UI/Footers';
+
+const AppLayout = () => {
+  return <>
+    <Headers />
+    <Footers />
+
+  </>
+};
+
+export default AppLayout;
