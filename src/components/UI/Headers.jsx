0eb7f7065be81287e20a566be9921fd735b9bdf7
Rajan Vaishnav <1574@silvertouch.local>
2025-02-26 15:47:27 +0530

    header and menu update

--- src/components/UI/Headers.jsx
+++ src/components/UI/Headers.jsx
@@ -1,3 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+
+
 export const Headers = () => {
-  return <h1>Hello Headers</h1>
+  return (     
+        <header>
+
+            <div className="container">
+                
+                <div className="grid navbar-grid">
+                  <div className="Logo">
+                      <NavLink to="/" >
+                    <h1>WorldAtlas</h1>
+
+                    </NavLink>
+                  </div>
+                  <nav>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/About">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Contact">Contact</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Country">Country</NavLink>
+                        </li>
+                     
+                    </ul>
+                </nav>
+                </div>
+
+            
+           
+            </div>
+        </header>
+  )
 };
